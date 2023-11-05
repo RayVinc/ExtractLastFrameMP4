0dbee113ed10007073870caf6f605fe91c29210b
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-11-05 15:20:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 from tkinter import filedialog
 import moviepy.editor as mp
+from pathlib import Path
 
 def extract_audio():
     file_path = filedialog.askopenfilename(filetypes=[("MP4 files", "*.mp4")])
     
     if file_path:
         video = mp.VideoFileClip(file_path)
-        audio = video.audio
-        audio.write_audiofile("extracted_audio.mp3")
-        audio.close()
-        video.close()
-        status_label.config(text="Audio extraction complete!")
+        
+        save_path = filedialog.asksaveasfilename(defaultextension=".mp3", filetypes=[("MP3 files", "*.mp3")])
+        
+        if save_path:
+            audio = video.audio
+            audio.write_audiofile(save_path)
+            audio.close()
+            video.close()
+            status_label.config(text="Audio extraction complete!")
 
 # Create the main application window
 app = tk.Tk()
