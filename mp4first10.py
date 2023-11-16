b4d591111095fe83f1bbe1be70c37fb598a88364
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-11-16 13:04:56 +0000

    Add files via upload

---
+++ mp4first10.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import filedialog
+from moviepy.video.io.VideoFileClip import VideoFileClip
+
+class VideoExtractorApp:
+    def __init__(self, master):
+        self.master = master
+        master.title("Video Extractor")
+
+        self.file_path = tk.StringVar()
+
+        self.label = tk.Label(master, text="Select MP4 Video:")
+        self.label.pack(pady=10)
+
+        self.browse_button = tk.Button(master, text="Browse", command=self.browse_file)
+        self.browse_button.pack(pady=10)
+
+        self.extract_button = tk.Button(master, text="Extract First 10 Seconds", command=self.extract_video)
+        self.extract_button.pack(pady=10)
+
+    def browse_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("MP4 files", "*.mp4")])
+        self.file_path.set(file_path)
+
+    def extract_video(self):
+        input_file = self.file_path.get()
+
+        if not input_file:
+            tk.messagebox.showerror("Error", "Please select a valid MP4 file.")
+            return
+
+        output_file = filedialog.asksaveasfilename(defaultextension=".mp4", filetypes=[("MP4 files", "*.mp4")])
+
+        if output_file:
+            with VideoFileClip(input_file) as video:
+                video_clip = video.subclip(0, 10)
+                video_clip.write_videofile(output_file, codec="libx264", audio_codec="aac")
+
+            tk.messagebox.showinfo("Success", f"Extraction completed successfully. Output saved as {output_file}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = VideoExtractorApp(root)
+    root.mainloop()
